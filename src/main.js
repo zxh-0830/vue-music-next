2076ed2e0fba217587ac6afbd9e156733b5b2cf8
zhuangxinghua <1287712468@qq.com>
2021-07-14 23:37:31 +0800

    【优化】 热门推荐列表图片懒加载

--- src/main.js
+++ src/main.js
@@ -4,5 +4,8 @@ import router from './router'
 import store from './store'
 // 引入全局样式文件
 import '@/assets/scss/index.scss'
+import lazyPlugin from 'vue3-lazy'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(lazyPlugin, {
+  loading: require('@/assets/images/default.png')
+}).mount('#app')
